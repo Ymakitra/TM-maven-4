ae3d534e35563965f4100adcdbe226c5a9a56400
vfitsyk <vfitsyk@gmail.com>
2016-08-08 22:34:16 +0300

    Added try/catch blocks to ReadFromFile.java and TM.java

--- src/main/java/ReadFromFile.java
+++ src/main/java/ReadFromFile.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
+//import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,17 +8,21 @@ import java.util.Scanner;
 
 
 public class ReadFromFile {
-    public static void main(String[] args) throws IOException {
-        String text;
-//        System.out.println(ToArrray());
+    public static void main(String[] args){// throws IOException {
+        //String text;
         String s = ToArrray();
-
-        List<String> myList = new ArrayList<>(Arrays.asList(s.split(":")));
+        List<String> myList = new ArrayList<String>(Arrays.asList(s.split(":")));
         System.out.println(myList);
 
     }
-    private static String ToArrray() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("C:\\IDEA\\V3\\src\\main\\java\\Data.txt"));
+
+    private static String ToArrray() {//throws FileNotFoundException {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("C:\\IDEA\\V3\\src\\main\\java\\Data.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         String text = scanner.useDelimiter("\\A").next();
         scanner.close();
 //        System.out.println(text);
--- src/main/java/TM.java
+++ src/main/java/TM.java
@@ -1,10 +1,16 @@
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+//import java.io.IOException;
 import java.util.Scanner;
 
 public class TM {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(new File("C:\\IDEA\\V3\\src\\main\\javaData.txt"));
+    public static void main(String[] args) {//throws IOException {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("C:\\IDEA\\V3\\src\\main\\javaData.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         String text = scanner.useDelimiter("\\A").next();
         scanner.close();
         System.out.println(text);
