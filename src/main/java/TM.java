4f981e444313aa2ffeb981f30d4d3fbaeb712626
omela <oomelyanchuk@gmail.com>
2016-08-14 16:30:07 +0400

    modify TM class

--- src/main/java/TM.java
+++ src/main/java/TM.java
@@ -1,19 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 //import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TM {
-    public static void main(String[] args) {
-        Scanner scanner = null;
+    public List<String> File(String s) {
+        List<String> ln = new ArrayList<String>();
+       // Scanner scanner = null;
         try {
-            scanner = new Scanner(new File("src/main/javaData.txt"));
+            File file = new File(s);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                ln.add(scanner.next());
+            }
+            scanner.close();
         } catch (FileNotFoundException e) {
             //e.printStackTrace();
             System.out.println("Reading file error!");
         }
-        String text = scanner.useDelimiter("\\A").next();
-        scanner.close();
-        System.out.println(text);
+       // String text = scanner.
+                //useDelimiter("\\A").next();
+        return ln;
+       // scanner.close();
+        //System.out.println(text);
     }
 }
